f1a1ed4ce5c344041c9317cb50ded2ce0c614a74
Khaled <me@khaledxyz.com>
2024-04-30 18:01:15 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
 app.get('/',
-    (req, res) => res.send('Dockerizing Node Application v4'))
+    (req, res) => res.send('Dockerizing Node Application v5'))
 
 app.listen(5000,
     () => console.log(`⚡️[bootup]: Server is running at port: 5000`));
