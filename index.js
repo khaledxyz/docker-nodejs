7e1a6a0edcb241297d1f6947093a0ee887fcc662
Khaled <me@khaledxyz.com>
2024-04-30 16:31:46 +0100

    updata to v1

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
 app.get('/',
-    (req, res) => res.send('Dockerizing Node Application v0'))
+    (req, res) => res.send('Dockerizing Node Application v1'))
 
 app.listen(5000,
     () => console.log(`⚡️[bootup]: Server is running at port: 5000`));
